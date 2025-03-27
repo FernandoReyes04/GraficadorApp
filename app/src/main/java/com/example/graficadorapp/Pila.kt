a23cdb3a794738022b2b966ede32bf0cc076f340
Valeria-Lee <leev6893a@gmail.com>
2025-03-27 08:22:25 -0600

    :feat: :sparkles: pila terminada, falta probar

--- app/src/main/java/com/example/graficadorapp/Pila.kt
+++ app/src/main/java/com/example/graficadorapp/Pila.kt
@@ -1,17 +1,36 @@
 package com.example.graficadorapp
 
-class Pila(val tam:Int, var cima:Int, var elementos:Array<Char>) {
-    fun crearPila(elementos:Array<Char>) {} // TODO: metodo crear pila
+// el tamano es el valor maximo de elementos que puede alcanzar.
+// la cima es el indice del ultimo elemento.
+class Pila(val tam:Int, var elementos:Array<Char>) {
+    private var cima = elementos.size
 
-    fun insertar(elemento: Char) {
-        if (elementos.size < tam) {
-            // entonces, añadir elemento.
-            cima++ // se incrementa el "indice"
-            elementos.plus(elemento) // se anade.
+    fun crearPila(e:Array<Char>) {
+        if (e.size < tam) {
+            elementos = e
+            cima = e.size
         } else {
-            println("No se pudo añadir el elemento :(")
+            println("La pila solo va a tomar hasta los ultimos $tam elementos")
+            elementos = e.takeLast(tam).toTypedArray() // se achica la pila para que no haya desbordamiento
         }
     }
 
-    fun quitar() {} // TODO: metodo quitar elemento.
+    fun insertar(elemento: Char) {
+        if (elementos.size < tam) {
+            cima++
+            elementos.plus(elemento)
+        } else {
+            println("La pila esta llena.")
+        }
+    }
+
+    fun quitar() {
+        if (cima > 0) {
+            cima--
+            // quita el primer elemento, convierte a un array en caso de ser convertido a lista.
+            elementos.drop(1).toTypedArray()
+        } else {
+            println("La pila esta vacia.")
+        }
+    }
 }
